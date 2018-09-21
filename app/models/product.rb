ee45c3f77b8fb450a1af5cb9df0d424d5d8a752a
Nate Cottle <cottle.nate@gmail.com>
2018-09-21 12:14:34 -0700

    Allow for product price to be other than whole number amounts, forms for new and edit products now work with decimals.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
-  validates :price, numericality: { only_integer: true, greater_than: 0}
+  validates :price, numericality: { greater_than: 0}
   validates :image, presence: true
 end
