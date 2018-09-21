f226c4c4695a9b7bdc3b4aecf2df6e7287a19ab0
Nate Cottle <cottle.nate@gmail.com>
2018-09-21 10:56:48 -0700

    Add delete functionality with ajax, cart items and total price displays update.

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ To set up:
 ### Contributions
 
 1. Add Ajax to products#index page to take form input and update cart without refresh.
+1. Product details are hidden and can be displayed using Ajax by clicking on details in the product card. 
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -25,7 +25,10 @@ class OrderItemsController < ApplicationController
     @item = @order.order_items.find(params[:id])
     @item.destroy
     @order.save
-    redirect_to cart_path
+    respond_to do |format|
+      format.html { redirect_to carts_path }
+      format.js
+    end
   end
 
   private
